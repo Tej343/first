9cd5bdd2ace513e07f97e9c71487a653e5d4fd88
Tej343 <madugula.saitej@gmail.com>
2026-02-02 19:24:50 +0530

    Update AI_Agent.py

--- AI_Agent.py
+++ AI_Agent.py
@@ -218,8 +218,9 @@ if uploaded_file:
                 df['Total GST'] = df['Total GST'].str.replace(',', '').astype(float)
                 df['Grand Total'] = df['Grand Total'].str.replace(',', '').astype(float)
 
-                df.loc[df['Value'].isna(), 'Total GST'] = 0
-                df.loc[df['Value'].isna(), 'Grand Total'] = 0
+                df = df[~df['Value'].isna()]
+                # df.loc[df['Value'].isna(), 'Total GST'] = 0
+                # df.loc[df['Value'].isna(), 'Grand Total'] = 0
                 
                 total_value = df['Value'].sum()
                 df['Total GST'] = df['Total GST']/2
